ab7eefe059ce6288c6ae4fddb1f437d1cce53a47
hossein vkh <hosseinvkhdev@gmail.com>
2025-03-05 10:06:06 +0330

    feat: fix bug

--- public/js/script.js
+++ public/js/script.js
@@ -8,3 +8,19 @@ function navToggle() {
   menu.classList.toggle("flex");
   menu.classList.toggle("hidden");
 }
+
+const btnFaq = document.querySelectorAll(".btn-faq");
+const txtFaq = document.querySelectorAll(".faq-text");
+btnFaq.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    txtFaq.forEach((div, divIndex) => {
+      console.log(div, divIndex, index);
+      if (divIndex === index) {
+        div.classList.toggle("hidden");
+        div.classList.toggle("flex");
+        // Add the 'active' class to the clicked div
+      }
+    });
+  });
+});
+
